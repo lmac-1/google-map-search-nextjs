2bf47506e5581df1a9f95dec99d8e1a4589439a7
Lucy Macartney <macartney.lucy@gmail.com>
2024-01-11 12:53:55 -0500

    first commit

--- README.md
+++ README.md
@@ -1,36 +1,31 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Welcome
 
-## Getting Started
+This is a simple repository used to follow the "Build Interactive Maps in Next.js using Google Maps API" tutorial
 
-First, run the development server:
+Link: https://www.99darshan.com/posts/interactive-maps-using-nextjs-and-google-maps
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+## Packages
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+- `@react-google-maps/api`: [Repo](https://github.com/JustFly1984/react-google-maps-api/)
+- `use-places-autocomplete`: [Repo](https://github.com/wellyshen/use-places-autocomplete)
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+## Setup
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+1. Create an API key in the Google Cloud Console which allows the following APIs:
 
-## Learn More
+   - Geocoding API
+   - Maps JavaScript API
+   - Places API
 
-To learn more about Next.js, take a look at the following resources:
+2. Create a `.env.local` file with `NEXT_PUBLIC_GOOGLE_MAPS_KEY=` followed by your API key
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+## PlacesAutocomplete
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+When using `usePlacesAutocomplete`:
 
-## Deploy on Vercel
+- `debounce`: specifies the numbers of milliseconds to delay before making a request to Google Maps Places API
+- `cache`: specifies the number of seconds to cache the response data from the Google Maps Places API
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+## Todo:
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Need to improve the accessibility of the google map autocomplete
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,94 @@
-import Image from "next/image";
+"use client";
+import {
+  //CircleF,
+  GoogleMap,
+  MarkerF,
+  useLoadScript,
+} from "@react-google-maps/api";
+import { useMemo, useState } from "react";
+import { PlacesAutocomplete } from "./components/PlacesAutocomplete";
+import { getGeocode, getLatLng } from "use-places-autocomplete";
+
+const DEFAULT_LOCATION = { lat: 37.98, lng: 23.72 };
 
 export default function Home() {
-  return <main>Hello world</main>;
+  const [lat, setLat] = useState(DEFAULT_LOCATION.lat);
+  const [lng, setLng] = useState(DEFAULT_LOCATION.lng);
+
+  const libraries = useMemo(() => ["places"], []);
+  // This gives coordinates for the center of the map
+  const mapCenter = useMemo(() => ({ lat, lng }), [lat, lng]);
+
+  const mapOptions = useMemo<google.maps.MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: true,
+      scrollwheel: false,
+    }),
+    []
+  );
+
+  // This loads the google maps script into our application
+  const { isLoaded } = useLoadScript({
+    // your API key will be publicly exposed, so be sure to apply HTTP restrictions on the Google Cloud console
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY as string,
+    // This specifies the google maps libraries that we want to load (e.g. we could add drawing, geometry, places etc)
+    libraries: libraries as any,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <main className="flex justify-center align-center">
+      <div className="border border-r-gray-400 w-1/4 h-screen bg-gray-200">
+        <PlacesAutocomplete
+          onAddressSelect={(address) => {
+            getGeocode({ address }).then((results) => {
+              const { lat, lng } = getLatLng(results[0]);
+
+              setLat(lat);
+              setLng(lng);
+            });
+          }}
+        />
+      </div>
+      <GoogleMap
+        options={mapOptions}
+        zoom={14}
+        center={mapCenter}
+        mapTypeId={google.maps.MapTypeId.ROADMAP}
+        mapContainerClassName="w-3/4"
+        //mapContainerStyle={{ width: "800px", height: "800px" }}
+        onLoad={() => console.log("Map component loaded")}
+      >
+        {/* This draws a marker over the map
+          position prop specifies the lat + long of where to place ther marker
+        */}
+        <MarkerF
+          position={mapCenter}
+          onLoad={() => console.log("Marker loaded")}
+          /* you can also change the icon of the marker 
+          icon="https://picsum.photos/64"*/
+        />
+        {/* we can also draw circles around the map */}
+        {/* {[1000, 2500].map((radius, idx) => {
+          return (
+            <CircleF
+              key={idx}
+              center={mapCenter}
+              radius={radius}
+              onLoad={() => console.log("Circle Load...")}
+              options={{
+                fillColor: radius > 1000 ? "red" : "green",
+                strokeColor: radius > 1000 ? "red" : "green",
+                strokeOpacity: 0.8,
+              }}
+            />
+          );
+        })} */}
+      </GoogleMap>
+    </main>
+  );
 }
